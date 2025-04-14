8e5c9ce3a4cacf6699eaa17d3be55085869ffe25
RiversDupaquier <rdupaq4@lsu.edu>
2025-04-14 16:10:46 -0500

    changes

--- jackblack/lib/main.dart
+++ jackblack/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   //Jessica Chan
   // Gayoon Nam
   //Lynn Nguyen
+  //Rivers Dupaquier
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
