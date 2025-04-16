fe9f3faad7d4180ca9b5c72797f4ec99ff98d786
Cleveland Martin IV <tsarycleveland@gmail.com>
2025-04-16 16:06:57 -0500

    edit p2p page

--- jackblack/lib/peertopeer.dart
+++ jackblack/lib/peertopeer.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Peer to Peer',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -36,9 +36,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Peer To Peer Test'),
     );
   }
 }
@@ -92,6 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
