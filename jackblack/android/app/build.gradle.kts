d0ed922043d6cebe8920e8e31e69d52979fd0f0d
Cleveland Martin IV <tsarycleveland@gmail.com>
2025-04-20 16:29:04 -0500

    I think p2p is set up and readu to implement?

--- jackblack/android/app/build.gradle.kts
+++ jackblack/android/app/build.gradle.kts
@@ -8,15 +8,20 @@ plugins {
 android {
     namespace = "com.example.jackblack"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
+    // compileOptions {
+    //     sourceCompatibility = JavaVersion.VERSION_11
+    //     targetCompatibility = JavaVersion.VERSION_11
+    // }
+
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
