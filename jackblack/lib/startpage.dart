0ecd3855371353a4c385250e46935a4bbc84a965
jennixen <143552870+jennixen@users.noreply.github.com>
2025-04-21 13:54:05 -0500

    Added button to startpage.dart to reroute to choose mode page

--- jackblack/lib/startpage.dart
+++ jackblack/lib/startpage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'modepage.dart';
 
 void main() {
-  runApp(const StartPage());
+  runApp(const StartPageEntry());
 }
 
-class StartPage extends StatelessWidget {
-  const StartPage({super.key});
+class StartPageEntry extends StatelessWidget {
+  const StartPageEntry({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,16 @@ class StartPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("Jack Black Blackjack"),
+              const Text("Jack Black Blackjack"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ModePage()),
+                  );
+                },
+                child: const Text("Start Game"),
+              ),
             ],
         ))
       ),
