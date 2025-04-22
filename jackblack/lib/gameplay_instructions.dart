f3c6cffb84786ea0ccbe9887ba821f6dda12628c
RiversDupaquier <rdupaq4@lsu.edu>
2025-04-22 10:06:23 -0500

    how betting works

--- jackblack/lib/gameplay_instructions.dart
+++ jackblack/lib/gameplay_instructions.dart
@@ -26,49 +26,86 @@ class GameplayInstructions extends StatelessWidget {
             ),
             SizedBox(height: 16),
             Text(
-              '1. The goal of Blackjack is to beat the dealer\'s hand without going over 21.',
+              '- The goal of Blackjack is to beat the dealer\'s hand without going over 21.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '2. Face cards (Kings, Queens, and Jacks) are worth 10 points. Aces are worth 1 or 11 points, whichever makes a better hand.',
+              '- Kings, Queens, and Jokers are worth 10 points. Aces are worth 11 points.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '3. Each player starts with two cards, and the dealer also gets two cards, with one card face up and one face down.',
+              '- Each player and the dealer starts with two cards. The dealer will have one card face up and one card face down.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '4. On your turn, you can choose to "Hit" (take another card) or "Stand" (keep your current hand).',
+              '- On your turn, you can choose to "Hit" (take another card) or "Stand" (keep your current hand).',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '5. If your hand exceeds 21, you "Bust" and lose the round.',
+              '- If your hand exceeds 21, you "Bust" and lose the round.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '6. The dealer must hit until their cards total 17 or higher.',
+              '- The dealer must hit until their cards total 17 or higher.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
             ),
             SizedBox(height: 8),
             Text(
-              '7. If you beat the dealer\'s hand without busting, you win!',
+              '- If you beat the dealer\'s hand without busting, you win the round.',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Minecraft',),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '- If you and the dealer have the same hand, it\'s a "Push" and no one wins the round.',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Minecraft',),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '- If you run out of chips, you lose the game.',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Minecraft',),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'How Betting works',
+              style: TextStyle(
+                fontFamily: 'Minecraft',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '- Players start with a chip that represents how much money they have',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'Minecraft',),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '- Before the start of the game, each player must bet their chips. Bets cannot fall below the minimum betting limit or go above the maximum betting limit.',
               style: TextStyle(
                 fontSize: 16,
                 fontFamily: 'Minecraft',),
