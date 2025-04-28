f12cb0a9b216e679d80336a221e7776223b3665b
Demir Ekal <Dekal1@lsu.edu>
2025-04-27 19:48:28 -0500

    Change the button styling.

--- jackblack/lib/modepage.dart
+++ jackblack/lib/modepage.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jackblack/game.dart';
-
-void main() {
-  runApp(const MaterialApp(home: ModePage()));
-}
+import 'package:jackblack/titlepage.dart';
+import 'package:jackblack/widgets/custom_button.dart';
 
 class ModePage extends StatelessWidget {
   const ModePage({super.key});
@@ -17,63 +15,48 @@ class ModePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              "Select Game Mode",
+              "Select\nGame Mode",
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 40,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
                 fontFamily: 'Minecraft',
+                shadows: [Shadow(offset: Offset(5, 5), blurRadius: 0, color: Color.fromRGBO(63, 63, 63, 1))]
               ),
             ),
             const SizedBox(height: 40),
-            buildModeButton(
-              context,
-              label: "Play Against Dealer",
+            CustomButton(
+              text: "Play Against Dealer",
               onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => GamePage()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const GamePage()));
               },
             ),
-            const SizedBox(height: 30),
-            buildModeButton(
-              context,
-              label: "Join a Game with Virtual Cards",
-              onPressed: () {},
+            const SizedBox(height: 25),
+            CustomButton(
+              text: "Host Game",
+              width: 302,
+              onPressed: () {
+                // To host game screen
+              },
             ),
-            const SizedBox(height: 30),
-            buildModeButton(
-              context,
-              label: "Join a Game with Physical Cards",
-              onPressed: () {},
+            const SizedBox(height: 25),
+            CustomButton(
+              text: "Join Game",
+              width: 302,
+              onPressed: () {
+                // To join game screen
+              },
+            ),
+            const SizedBox(height: 25),
+            CustomButton(
+              text: "Back",
+              width: 302,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const TitlePage()));
+              },
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildModeButton(BuildContext context,
-      {required String label, required VoidCallback onPressed}) {
-    return SizedBox(
-      width: 260,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromRGBO(198, 255, 202, 1),
-          foregroundColor: const Color.fromRGBO(0, 0, 0, 1),
-          padding: const EdgeInsets.symmetric(vertical: 12),
-          textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-        child: Center(
-          child: Text(
-            label,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontFamily: 'Minecraft'),
-          ),
         ),
       ),
     );
