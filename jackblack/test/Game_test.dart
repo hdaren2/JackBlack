3ae7cea0ab62078acf075c9e929736b3a81db010
Derekv141 <157168869+Derekv141@users.noreply.github.com>
2025-05-06 21:46:26 -0500

    test2
    test for custom buttons

--- jackblack/test/Game_test.dart
+++ jackblack/test/Game_test.dart
@@ -22,5 +22,32 @@ void main() {
 
       // Add your test logic here using the public interface
     });
+
+    testWidgets('Hit customButton test', (WidgetTester tester) async {
+      final testkey1 = Key("Hit_custom");
+      bool buttonPressed = false;
+      await tester.pumpWidget(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Wrap(
+              spacing: 15,
+              runSpacing: 15,
+              alignment: WrapAlignment.center,
+              children: [CustomButton(text: "Hit", onPressed: () {})],
+            ),
+          ],
+        ),
+      );
+      // Verify the label is displayed
+      expect(find.text('Hit'), findsOneWidget);
+
+      // Tap the button and verify the onPressed callback is triggered
+      await tester.tap(find.byKey(testkey1));
+      await tester.pump(); // Rebuild the widget
+
+      expect(buttonPressed, isTrue);
+    });
   });
+  //end of test case
 }
